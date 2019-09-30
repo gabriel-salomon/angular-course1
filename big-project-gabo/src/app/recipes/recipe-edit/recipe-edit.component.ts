12103d4e485b10967ea2d26e21eb5d02a7da5eb0
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-09-30 06:58:12 -0400

    The save button now works, but if you go to the shopping-list and go back to the recipe book you recipe is destroyed I have to fix this error.

--- big-project-gabo/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ big-project-gabo/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -29,11 +29,15 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    const newRecipe = new Recipe(
-      this.recipeForm.value['name'],
-      this.recipeForm.value['description']);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']);
     if (this.editMode) {
-      this.recipeService.updateRecipe(this.id)
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
     }
   }
 
