cc4e82081d27bc671418af322a6bf6a052170c9c
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-10-06 07:33:16 -0400

    I added some stuff. This was added yesterday, but i forgot to commit it

--- http-01-gabo/src/app/app.component.ts
+++ http-01-gabo/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+import {Post} from "./post.model";
 
 @Component({
   selector: 'app-root',
@@ -11,13 +13,15 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts()
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+      .post<{name: string}>(
+        'https://ng-gabo.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +31,31 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>('//ng-gabo.firebaseio.com/posts.json')
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts =>{
+        // ...
+        this.loadedPosts
+      });
+  }
 }
+
