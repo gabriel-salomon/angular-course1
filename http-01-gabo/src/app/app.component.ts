04d4c85a801404926c82b979fbfe49e01de2f867
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-10-07 06:52:01 -0400

    I added stuff in case you have in error.

--- http-01-gabo/src/app/app.component.ts
+++ http-01-gabo/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Subscription} from "rxjs";
 
 import {Post} from "./post.model";
 import {PostsService} from "./posts.service";
@@ -9,19 +10,29 @@ import {PostsService} from "./posts.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
+    this.postsService.fetchPosts().subscribe(
+      posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    });
+    },
+      error => {
+      this.error = error.message;
+    }
+    );
   }
 
   onCreatePost(postData: Post) {
@@ -32,12 +43,16 @@ export class AppComponent implements OnInit {
   onFetchPosts() {
     // Send Http request
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
+    this.postsService.fetchPosts().subscribe(
+      posts =>
+      {this.isFetching = false;
       this.loadedPosts = posts;
-    }, error => {
-
-    });
+    },
+      error => {
+        this.error = error.message;
+        console.log(error);
+      }
+      );
   }
 
   onClearPosts() {
@@ -45,6 +60,10 @@ export class AppComponent implements OnInit {
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
 
