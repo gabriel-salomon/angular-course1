3b248019020a3c5cab25014c2a82d096cace05c8
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-10-20 10:02:05 -0400

    I added a resolver and fixed some buga.

--- big-project-gabo/src/app/shared/data-storage.service.ts
+++ big-project-gabo/src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import {RecipeService} from "../recipes/recipe.service";
-import {Recipe} from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -21,11 +22,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
-      .get<Recipe[]>('https://big-project-gabo.firebaseio.com/recipes.json')
-      .subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
-    });
+    return this.http
+      .get<Recipe[]>(
+        'https://big-project-gabo.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+         });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
 
