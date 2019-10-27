e08893bcc9cc088d5f6a63597af27cc48983b157
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-10-27 10:57:43 -0400

    I started working on singing up.

--- big-project-gabo/src/app/auth/auth.service.ts
+++ big-project-gabo/src/app/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
 @Injectable({providedIn: "root"})
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    this.http.post(
+    return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA6_Hu_6qnO2s48IAAwKKu2uBzeMWSy5e8',
      {
        email: email,
