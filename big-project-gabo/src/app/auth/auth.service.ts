2867c3417672a01e39ffeb6a4d269974a0c9e18c
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-10-27 13:04:39 -0400

    You can now login, and i started working on error handling for my login method.

--- big-project-gabo/src/app/auth/auth.service.ts
+++ big-project-gabo/src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError} from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: "root"})
@@ -24,7 +25,8 @@ export class AuthService {
        password: password,
        returnSecureToken: true
      }
-    ).pipe(catchError(errorRes => {
+    ).pipe(
+      catchError(errorRes => {
       let errorMessage = 'An unknown error occurred';
       if (!errorRes.error || !errorRes.error.error) {
         return throwError(errorMessage);
@@ -34,6 +36,30 @@ export class AuthService {
           errorMessage = 'This email exists already';
       }
       return throwError(errorMessage);
-    }));
+    })
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA6_Hu_6qnO2s48IAAwKKu2uBzeMWSy5e8',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS' :
+        errorMessage = 'This email exists already';
+    }
+    return throwError(errorMessage);
   }
 }
