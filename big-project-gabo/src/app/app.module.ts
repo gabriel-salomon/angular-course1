53db04453e54fe65ecadae59cfc5beb995608140
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2019-11-10 11:34:15 -0500

    I finished with the alert component. If you have any error while login in or signing up, a box will pop up, and you can close it now.

--- big-project-gabo/src/app/app.module.ts
+++ big-project-gabo/src/app/app.module.ts
@@ -61,7 +61,10 @@ import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
       multi: true
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule {}
 
--- big-project-gabo/src/app/auth/auth.component.ts
+++ big-project-gabo/src/app/auth/auth.component.ts
@@ -1,21 +1,28 @@
-import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { AlertComponent } from '../shared/alert/alert.component';
-import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy{
   isLoginMode = true;
   isLoading = false;
   error: string = null;
   @ViewChild(PlaceholderDirective, { static: false } ) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -65,6 +72,12 @@ export class AuthComponent {
     this.error = null;
   }
 
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
+
   private showErrorAlert(message: string) {
    // const alertCmp = new AlertComponent();
    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(
@@ -73,6 +86,12 @@ export class AuthComponent {
    const hostViewContainerRef = this.alertHost.viewContainerRef;
    hostViewContainerRef.clear();
 
-   hostViewContainerRef.createComponent(alertCmpFactory);
+   const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+   componentRef.instance.message = message;
+   this.closeSub = componentRef.instance.close.subscribe(() => {
+     this.closeSub.unsubscribe();
+     hostViewContainerRef.clear();
+   });
   }
 }
