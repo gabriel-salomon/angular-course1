fbaa380b1b8f684e1ac70600c5ed07cd99516ef1
gabriel <gabriel.salomon.2007@gmail.com>
2019-11-30 10:49:12 -0500

    i am doing animations.

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -14,7 +14,7 @@ import {
     trigger('divState', [
       state('normal', style({
         'background-color': 'red',
-        transform: 'translateX0'
+        transform: 'translateX(0)'
       })),
       state('highlighted', style({
         'background-color': 'blue',
@@ -22,15 +22,38 @@ import {
       })),
       transition('normal <=> highlighted', animate(300)),
       // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', )),
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   OnAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  OnShrink() {
+    this.wildState = 'shrunken';
   }
 
     onAdd(item) {
