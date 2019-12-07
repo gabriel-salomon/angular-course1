0fb8e77ba3ee63bdf97c435cef2f591ad27b778a
gabriel <gabriel.salomon.2007@gmail.com>
2019-12-07 13:00:43 -0500

    I did way more testing.

--- testing-gabo/src/app/user/user.component.spec.ts
+++ testing-gabo/src/app/user/user.component.spec.ts
@@ -9,14 +9,17 @@ describe('Component: User', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [UserComponent]
-    });
-  });
+    }
+    );
+  }
+  );
 
   it('should create the app', () => {
     let fixture = TestBed.createComponent(UserComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }
+  );
 
   it('should use the user name from the service', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -24,7 +27,8 @@ describe('Component: User', () => {
     let userService = fixture.debugElement.injector.get(UserService);
     fixture.detectChanges();
     expect(userService.user.name).toEqual(app.user.name);
-  });
+  }
+  );
 
   it('should display the user name if user is logged in', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -33,7 +37,8 @@ describe('Component: User', () => {
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).toContain(app.user.name);
-  });
+  }
+  );
 
   it('shouldn\'t display the user name if user is not logged in', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -41,7 +46,8 @@ describe('Component: User', () => {
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
-  });
+  }
+  );
 
   it('shouldn\'t fetch data successfully if not called asynchronously', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -51,7 +57,8 @@ describe('Component: User', () => {
       .and.returnValue(Promise.resolve('Data'));
     fixture.detectChanges();
     expect(app.data).toBe(undefined);
-  });
+  }
+  );
 
   it('should fetch data successfully if called asynchronously', async(() => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -62,8 +69,11 @@ describe('Component: User', () => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(app.data).toBe('Data');
-    });
-  }));
+    }
+    );
+  }
+  )
+  );
 
   it('should fetch data successfully if called asynchronously', fakeAsync(() => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -75,5 +85,8 @@ describe('Component: User', () => {
     tick();
     expect(app.data).toBe('Data');
 
-  }));
-});
+  }
+  )
+  );
+}
+);
